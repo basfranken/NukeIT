25b18855c2e9a936a6de1c5d5979b91562668e5d
basfranken <b.franken@student.han.nl>
2021-04-16 08:47:47 +0200

    Added Maven Added Game Engine Game is runnable

---
+++ NukeITApp/src/main/java/nl/han/oopg/nukeit/NukeIT.java
@@ -0,0 +1,20 @@
+package nl.han.oopg.nukeit;
+
+import nl.han.ica.oopg.engine.GameEngine;
+
+public class NukeIT extends GameEngine {
+    public static void main(String[] args) {
+        NukeIT app = new NukeIT();
+        app.runSketch();
+    }
+
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
