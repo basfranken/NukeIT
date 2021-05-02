36cae103ed78448e18d26df693bdab7d5ca34334
Bas Franken <basf@live.nl>
2021-05-02 20:13:01 +0200

    Updated UML

--- NukeITApp/src/main/java/nl/han/oopg/nukeit/Classes/LifePowerUp.java
+++ NukeITApp/src/main/java/nl/han/oopg/nukeit/Classes/LifePowerUp.java
@@ -4,14 +4,16 @@ import nl.han.ica.oopg.collision.ICollidableWithGameObjects;
 import nl.han.ica.oopg.objects.GameObject;
 import nl.han.ica.oopg.objects.Sprite;
 import nl.han.ica.oopg.objects.SpriteObject;
+import nl.han.oopg.nukeit.AbstractClasses.PowerUp;
+
 import java.util.List;
 
-public class LifePowerUp extends SpriteObject implements ICollidableWithGameObjects {
+public class LifePowerUp extends PowerUp {
 
-    private NukeITWorld world;
+    private final NukeITWorld world;
 
     public LifePowerUp(NukeITWorld world, int size) {
-        super(new Sprite("NukeITApp/src/main/java/nl/han/oopg/nukeit/data/PowerUps/add life.png"));
+        super(new Sprite("NukeITApp/src/main/java/nl/han/oopg/nukeit/data/PowerUps/add life.png"), null, world);
         this.world = world;
         setWidth(size);
         setHeight(size);
--- NukeITApp/src/main/java/nl/han/oopg/nukeit/Classes/MinusSign.java
+++ NukeITApp/src/main/java/nl/han/oopg/nukeit/Classes/MinusSign.java
@@ -20,7 +20,6 @@ public class MinusSign extends SpriteObject{
         setHeight(100);
     }
 
-
     @Override
     public void update() {
 
